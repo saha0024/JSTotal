2cd5bd46df4aece7888ff0879d8287a1decbd32c
ParisaSaharkhiz <saha0024@algonquinlive.com>
2014-11-26 18:29:54 -0500

    JSTotal

---
+++ js/JSTotal.js
@@ -0,0 +1,4 @@
+//$("#btnOne").on("click", function() {
+//  $("<p>This is promotional material. Think of all the things that you can acheive if you use TotalJS.</p>").appendTo(".promotional");
+//});
+//
